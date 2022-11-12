9d27f297062e6807c9ae5868f4d15d81a4ac59bb
Aviatal <pbudek1908@gmail.com>
2022-11-12 21:00:56 +0100

    project's structure added

---
+++ view/layout.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../style/main.css">
+    </head>
+    <body>
+        <header>
+            Premier league
+        </header>
+    </body>
+</html>
